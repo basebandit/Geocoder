c4aadee0566f772e2a9c7b4f5826bb0ce84c951e
Evanson <evansonmwangi83@gmail.com>
2016-04-05 16:08:14 +0300

    Applicaton footer

---
+++ inc/components/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+        <!-- Copyright information -->
+        <address>
+            <p class="text-center"> Copyright &copy; Geocoder <?php echo date("Y"); ?> All rights reserved</p>
+        </address>
+    </div>
+</footer>
