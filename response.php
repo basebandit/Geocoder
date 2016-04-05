dbdde01ddbcf6d7c21e4f30d9682b965daf2d018
Evanson <evansonmwangi83@gmail.com>
2016-04-05 16:11:59 +0300

    Send output from server to client

---
+++ response.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script>
+    var result;
+    function reqListener() {
+        console.log(this.responseText);
+    }
+
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.onload = function () {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        result = this.responseText;
+        $.each(JSON.parse(result), function (index, obj) {
+            alert(obj.lng);
+        });
+
+    };
+    oReq.open("get", "csvtojson.php", true);
+    //                               ^ Don't block the rest of the execution.
+    //                                 Don't wait until the request finishes to
+    //                                 continue.
+    oReq.send();
+</script>
+</body>
+</html>
