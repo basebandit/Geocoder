6fbe77538ba01fb2660cb21c69a101d9a2309b75
Evanson <evansonmwangi83@gmail.com>
2016-04-05 16:03:39 +0300

    User help documentation and faq

---
+++ help.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="Geocoder">
+    <meta name="author" content="devmars">
+    <title>Geocoder::Help</title>
+
+    <!-- Bootstrap CSS file -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+    <link href="css/geocoder.css" rel="stylesheet">
+</head>
+<body>
+<?php require_once('inc/components/help_header.php'); ?>
+<?php require_once('inc/components/footer.php'); ?>
+</body>
+</html>
