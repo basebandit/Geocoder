d099d8e148b6631f4091eea574a144f88b40c0a6
Evanson <evansonmwangi83@gmail.com>
2016-04-08 13:58:57 +0300

    Add analysis output

--- js/geocode.js
+++ js/geocode.js
@@ -2,14 +2,18 @@ var map;
 var infoWindow;
 var boundRegion;
 var contentString;
-
-
+var boundRegionCoords;
+var areaName;
 function initMap() {
-    //Server side json output of the database query
+//Server side json output of the database query
     var result;
-
     //Region(polygon) to plot on the map
-    var boundRegionCoords = [
+//    boundRegionCoords = [{lat: 25.774, lng: -80.190},
+//        {lat: 18.466, lng: -66.118},
+//        {lat: 32.321, lng: -64.757},
+//        {lat: 25.774, lng: -80.190}
+//    ];
+    boundRegionCoords = [
         {lat: -1.0899350, lng: 37.0199040},
         {lat: -1.1179270, lng: 37.0247480},
         {lat: -1.1634730, lng: 37.0812830}
@@ -20,37 +24,51 @@ function initMap() {
         //This is where you handle what to do with the response.
         //The actual data is found on this.responseText
         result = this.responseText;
-        $.each(JSON.parse(result), function (index, obj) {
-            console.log(obj.lat + ":" + obj.lng);
-            arrayCoords.push(obj.lat, obj.lng);
+        //console.log(result.data);
+//        $.each(JSON.parse(result), function (index, obj) {
+//            //console.log(obj.lat + ":" + obj.lng);
+//            arrayCoords.push("lat:"+obj.lat, "lng:"+obj.lng);
+//            
+//        
+//
+//        });
+        //console.log($.parseJSON(result).data);
+        //boundRegionCoords = $.parseJSON(result).data;
+        $.each($.parseJSON(result).data, function (idx, obj) {
+            //console.log(obj.lat + ":" + obj.lng);   
+            boundRegionCoords[idx] = new google.maps.LatLng(obj.lat, obj.lng);
         });
+        areaName = $.parseJSON(result).area;
+        //console.log(boundRegionCoords);
+//        for (var i = 0; i < arrayCoords.length; i++) {
+//            // var coords = arrayCoords[i].split(",");
+//            // console.log(coords);
+//            var anotherArr = new Array();
+//            anotherArr[i] = new google.maps.LatLng(arrayCoords);
+//            //Array.prototype.splice.apply(boundRegionCoords, [0, boundRegionCoords.length].concat(anotherArr));
+//        }
+//        console.log(anotherArr);
     };
     oReq.open("get", "search.php", true);
     //                               ^ Don't block the rest of the execution.
     //                                 Don't wait until the request finishes to
     //                                 continue.
     oReq.send();
-
-    for (var i = 0; i < arrayCoords.length; i++) {
-        var coords = arrayCoords[i].split(",");
-        console.log(coords);
-        boundRegionCoords[i] = new google.maps.LatLng(coords[0], coords[1]);
-    }
-    console.log(boundRegionCoords);
+    //console.log(boundRegionCoords);
 
     //Wrapping my region coordinates in a bound so as
     //to get the center of the bound and start the
     //initial map plotting from the bound's center coordinates.
     var bound = new google.maps.LatLngBounds();
     for (var i = 0; i < boundRegionCoords.length; i++) {
-        bound.extend(new google.maps.LatLng(boundRegionCoords[i]));
+        bound.extend(new google.maps.LatLng(boundRegionCoords[i].lat, boundRegionCoords[i].lng));
     }
+//console.log(boundRegionCoords[0].lat);
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
-        center: bound.getCenter() /*{lat:-1.1267040000000001, lng:37.05059349999999}*/,
+        center: bound.getCenter()/*{lat:-1.1267040000000001, lng:37.05059349999999}*/,
         mapTypeId: google.maps.MapTypeId.HYBRID
     });
-
     //Construct the Polygon
     boundRegion = new google.maps.Polygon({
         paths: boundRegionCoords,
@@ -63,14 +81,22 @@ function initMap() {
     boundRegion.setMap(map);
     //Add a listener for the click event
     boundRegion.addListener('click', showArrays);
-
     infoWindow = new google.maps.InfoWindow;
-
     var geocoder = new google.maps.Geocoder();
-
-//    document.getElementById('submit').addEventListener('click', function () {
-//        geocodeAddress(geocoder, map);
+    $('#analyze').click(function () {
+        $("#area").append($.parseJSON(result).area);
+        $("#calcArea").val(calcArea());
+        $("#distance").append($.parseJSON(result).area);
+        $("#calcDistance").val(calcDistance());
+    });
+    window.onload = function () {
+        $("#calcArea").val("");
+        $("#calcDistance").val("");
+    };
+//    document.getElementById('analyze').addEventListener('click', function () {
+//
 //    });
+
 }
 
 function geocodeAddress(geocoder, resultsMap) {
@@ -89,36 +115,33 @@ function geocodeAddress(geocoder, resultsMap) {
 }
 /** @this {google.maps.Polygon} */
 function showArrays(event) {
-    //Since this boundRegion(Polygon) has only one path, we can call getPath() to return the
-    //MVCArray of LatLngs.
+//Since this boundRegion(Polygon) has only one path, we can call getPath() to return the
+//MVCArray of LatLngs.
     var vertices = this.getPath();
-
     contentString = '<b>Bound Region polygon</b><br>' +
-        'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
-        '<br>';
-
+            'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+            '<br>';
     //Iterate over the vertices.
     for (var i = 0; i < vertices.getLength(); i++) {
         var xy = vertices.getAt(i);
         contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
-            xy.lng();
+                xy.lng();
     }
     var area = calcArea();
     contentString += '<br>' + '<b>Area of Bound Region Polygon</b><br>' +
-        area.toFixed(1) + 'm<sup>2</sup>';
+            area.toFixed(1) + 'm<sup>2</sup>';
     var distance = calcDistance();
     contentString += '<br>' + '<b>Distance of Bound Region Polygon</b><br>' +
-        distance.toFixed(1) + 'm';
+            distance.toFixed(1) + 'm';
     //Replace the info Window's content and position.
     infoWindow.setContent(contentString);
     infoWindow.setPosition(event.latLng);
-
     infoWindow.open(map);
 }
 
 function calcArea() {
 
-    //Use the Google Maps geometry library to measure the area of the polygon(bound region)
+//Use the Google Maps geometry library to measure the area of the polygon(bound region)
     var area = google.maps.geometry.spherical.computeArea(boundRegion.getPath().getArray());
     return area;
 }
