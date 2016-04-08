0f6e71866d8f9192a3844a979c3b7216ba29121b
Evanson <evansonmwangi83@gmail.com>
2016-04-08 13:59:38 +0300

    Remove id element on navigation form

--- inc/components/header.php
+++ inc/components/header.php
@@ -18,14 +18,14 @@
             <a href="index.php" class="navbar-brand">Geocoder</a>
         </div>
         <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-            <form id="floating-panel" class="navbar-form navbar-left" action="search.php" role="search" method="post">
+            <form id="floating-panel" class="navbar-form navbar-left" action="search.php" role="search" method="POST">
                 <div class="form-group">
                     <!--  <input type="text" class="form-control" placeholder="Search Place">-->
                     <input id="address" class="form-control" type="text" placeholder="Enter search name"
                            name="address">
                 </div>
                 <!-- <button id="submit" type="submit" class="btn btn-default">Search</button>-->
-                <input id="submit" type="submit" class="btn btn-primary" name="submit" value="Search Place">
+                <input type="submit" class="btn btn-primary" name="submit" value="Search Place">
             </form>
             <ul class="nav navbar-nav navbar-right">
                 <li class="active"><a href="index.php"><span
